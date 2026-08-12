dbe955f6caea12b690342fa35a002a3da4b0e13f
Dusan <cankovicdusan4@gmail.com>
2026-08-12 17:39:34 +0200

    Fix next.config and ignore TS errors

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   cacheComponents: true,
-typescript: {
-  ignoreBuildErrors: true,
-},
-eslint: {
-  ignoreDuringBuilds: true,
-},
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default nextConfig;
